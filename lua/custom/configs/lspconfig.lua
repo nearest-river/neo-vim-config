1f65334d0c1dbb2d912e9e4cba808828cae0784b
Nate River <river177169@proton.me>
2025-12-19 12:41:39 +0530

    impl gopls

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,6 +1,7 @@
 local on_attach=require("plugins.configs.lspconfig").on_attach;
 local capabilities=require("plugins.configs.lspconfig").capabilities;
 local lspconfig=require("lspconfig");
+local util=require("lspconfig/util");
 local configs=require("plugins.configs.lspconfig");
 
 
@@ -30,5 +31,26 @@ lspconfig.pyright.setup({
   capabilities=capabilities,
   filetypes={"python"}
 });
+
+lspconfig.gopls.setup({
+  on_attach=on_attach,
+  capabilities=capabilities,
+  cmd={"gopls"},
+  filetypes={"go","gomod","gowork","gotmpl"},
+  root_dir=util.root_pattern("go.work","go.mod",".git"),
+  settings={
+    gopls={
+      completeUnimported=true,
+      usePlaceholders=true,
+      analyses={
+        unusedparam=true,
+      }
+    }
+  }
+});
+
+
+
+
 
 
